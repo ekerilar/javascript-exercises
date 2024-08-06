033ab3f990e0a8151f7909468e13a11f7c0e39b0
ekerilar <124172185+ekerilar@users.noreply.github.com>
2024-08-06 14:30:11 +0200

    completed assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+function removeFromArray(initialArray, ...valuesToRemove) {
+    let newArray = initialArray.filter(item => !valuesToRemove.includes(item));
+    return newArray;
+}
 
+    
 // Do not edit below this line
 module.exports = removeFromArray;
