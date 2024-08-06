121d8ced1eb6f034a88ee17115bd275a18489f12
ekerilar <124172185+ekerilar@users.noreply.github.com>
2024-08-06 12:10:34 +0200

    Assignment 1 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, numberTimes) {
+    let returnString = '';
+
+    if(numberTimes < 0){
+        return "ERROR";
+    }
+
+    for(let i = 1; i <= numberTimes; i++){
+       returnString = returnString.concat(inputString);
+    }
+    return returnString;
 
 };
 
