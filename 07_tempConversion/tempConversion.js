2d1405f28e2370a4ae92569bed638b34e3744761
ekerilar <124172185+ekerilar@users.noreply.github.com>
2024-08-07 09:52:12 +0200

    completed assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+
+  return parseFloat(((degreeF -32) * (5/9)).toFixed(1));
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+
+  return parseFloat(((degreeC * (9/5) + 32)).toFixed(1));
 };
 
 // Do not edit below this line
