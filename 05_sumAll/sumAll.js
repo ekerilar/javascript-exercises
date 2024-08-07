5715ba66265bc0ae45aa659c457549c99b7c240b
ekerilar <124172185+ekerilar@users.noreply.github.com>
2024-08-07 09:20:02 +0200

    completed assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let diff = 0;
+    let sum = 0;
 
+    if(number1 < 0 || number2 < 0 
+        || typeof number1 !== 'number' || typeof number2 !== 'number'
+        || !Number.isInteger(number1) || !Number.isInteger(number2)
+    ){
+        return "ERROR";
+    }
+
+    if(number1 > number2){
+        diff = number1 - number2;
+    } else {
+        diff = number2 - number1;
+    }
+
+    for (let i = 0; i <= diff + 1; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
